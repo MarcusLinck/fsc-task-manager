a27c5195d78c4254450b7e62e42733fae7faf22e
Marcus Linck <marcuslinck@gmail.com>
2025-03-21 08:25:00 -0300

    feat: add svg icons to Sidebar

--- src/components/SidebarButton.jsx
+++ src/components/SidebarButton.jsx
@@ -9,7 +9,10 @@ const SidebarButton = ({ children, variant }) => {
     }
   }
   return (
-    <a href="#" className={`rounded-lg px-6 py-3 ${getVariantClass()} `}>
+    <a
+      href="#"
+      className={`flex items-center gap-2 rounded-lg px-6 py-3 ${getVariantClass()} `}
+    >
       {children}
     </a>
   )
