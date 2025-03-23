528c15d829085851bb13cba2096ed5bbcd90d162
Marcus Linck <marcuslinck@gmail.com>
2025-03-23 09:27:30 -0300

    refactor: add tailwind-variants to SidebarButton component

--- src/components/SidebarButton.jsx
+++ src/components/SidebarButton.jsx
@@ -1,18 +1,17 @@
-const SidebarButton = ({ children, variant }) => {
-  const getVariantClass = () => {
-    if (variant === 'unselected') {
-      return 'text-brand-dark-blue'
-    }
+import { tv } from 'tailwind-variants'
+const SidebarButton = ({ children, color }) => {
+  const sidebar = tv({
+    base: 'flex items-center gap-2 rounded-lg px-6 py-3',
+    variants: {
+      color: {
+        selected: 'bg-brand-primary text-brand-primary bg-opacity-15',
+        unselected: 'text-brand-dark-blue',
+      },
+    },
+  })
 
-    if (variant === 'selected') {
-      return 'bg-brand-primary bg-opacity-15 text-brand-primary'
-    }
-  }
   return (
-    <a
-      href="#"
-      className={`flex items-center gap-2 rounded-lg px-6 py-3 ${getVariantClass()} `}
-    >
+    <a href="#" className={sidebar({ color })}>
       {children}
     </a>
   )
