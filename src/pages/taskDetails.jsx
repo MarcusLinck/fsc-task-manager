00ff5e7f28cba727acdb6cc20ea8647a3e4282a2
Marcus Linck <marcuslinck@gmail.com>
2025-03-25 17:26:36 -0300

    feat: add initial TaskDetails page without logic

--- src/pages/taskDetails.jsx
+++ src/pages/taskDetails.jsx
@@ -1,11 +1,20 @@
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 
+import { ArrowLeftIcon, ChevronRightIcon, TrashIcon } from '../assets/icons'
+import Button from '../components/Button'
+import Input from '../components/Input'
+import Select from '../components/Select'
 import Sidebar from '../components/Sidebar'
 
 const TaskDetailsPage = () => {
   const { taskId } = useParams()
   const [task, setTask] = useState()
+  const navigate = useNavigate()
+
+  const handleBackClick = () => {
+    navigate(-1)
+  }
 
   useEffect(() => {
     const fetchTask = async () => {
@@ -20,9 +29,62 @@ const TaskDetailsPage = () => {
   }, [taskId])
 
   return (
-    <div>
+    <div className="flex">
       <Sidebar />
-      <h1>{task?.title}</h1>
+      <div className="w-full space-y-6 px-8 py-16">
+        {/* barra do topo */}
+        <div className="flex w-full justify-between">
+          {/* parte da esquerda */}
+          <div>
+            <button
+              onClick={handleBackClick}
+              className="mb-3 flex h-8 w-8 items-center justify-center rounded-full bg-brand-primary"
+            >
+              <ArrowLeftIcon />
+            </button>
+            <div className="flex items-center gap-1 text-xs">
+              <Link className="cursor-pointer text-brand-text-gray" to="/">
+                Minhas tarefas
+              </Link>
+              <ChevronRightIcon className="text-brand-text-gray" />
+              <span className="font-semibold text-brand-primary">
+                {task?.title}
+              </span>
+            </div>
+
+            <h1 className="mt-2 text-xl font-semibold">{task?.title}</h1>
+          </div>
+
+          {/* parte da direita */}
+          <Button className="h-fit self-end" color="danger">
+            <TrashIcon />
+            Deletar tarefa
+          </Button>
+        </div>
+
+        <div>
+          {/* dados da tarefa */}
+          <div className="space-y-6 rounded-xl bg-brand-white p-6">
+            <div>
+              <Input id="title" label="Título" />
+            </div>
+
+            <div>
+              <Select />
+            </div>
+
+            <div>
+              <Input id="description" label="Descrição" />
+            </div>
+          </div>
+          {/* botão de salvar */}
+          <div className="flex w-full justify-end gap-3">
+            <Button size="large" color="primary" type="submit">
+              Salvar
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
